e076a8f93247aa5048b197e2afc7c19cbd627088
Tobi Kivelip <tkivelip@laramate.de>
2025-02-03 15:10:51 +0100

    Update base package versions

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,11 +6,6 @@ use Laramate\Support\Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * Example test.
-     *
-     * @test
-     */
     public function testExample()
     {
         $this->assertTrue(true);
