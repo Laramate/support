10941a6f98c8633109f5a53830ad8e0a65da8e40
Tobi Kivelip <tkivelip@laramate.de>
2026-03-31 16:37:28 +0200

    Add force json middleware

---
+++ src/Middleware/ForceJsonResponse.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Laramate\Support\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForceJsonResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $request->headers->set('Accept', 'application/json');
+
+        return $next($request);
+    }
+}
